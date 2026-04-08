325f564b3d08cd104c943e9fa13f6d61b9a362fb
ruri <ruriyoshinova@proton.me>
2026-04-09 07:45:26 +0800

    feat: add some assets

--- src/game/scenes/preload.ts
+++ src/game/scenes/preload.ts
@@ -27,6 +27,8 @@ export class Preloader extends Scene {
         this.load.setPath('assets');
 
         this.load.image('logo', 'logo.png');
+        this.load.svg('player', 'player.svg');
+        this.load.svg('bullet', 'bullet.svg');
     }
 
     create() {
